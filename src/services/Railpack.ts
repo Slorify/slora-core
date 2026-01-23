7d8976a671c4071421d8b261ee87e1359b5e00ae
aislxflames <proarghaop@gmail.com>
2026-01-23 17:41:14 +0530

    Update Railpack service implementation

--- src/services/Railpack.ts
+++ src/services/Railpack.ts
@@ -1,14 +1,23 @@
-import { runCmd } from "../utils/runCmd.js"
+import { runCmd } from "../utils/runCmd.js";
 
 class Railpack {
-
   async install(channel: string) {
-    runCmd(`curl -sSL https://railpack.com/install.sh | sh`, channel)
-    runCmd(`docker run --rm --privileged -d --name buildkit moby/buildkit`, channel,)
+    await runCmd(
+      `command -v railpack >/dev/null 2>&1 || curl -sSL https://railpack.com/install.sh | sh`,
+      channel,
+    );
+
+    await runCmd(
+      `docker ps --format '{{.Names}}' | grep -q '^buildkit$' || \
+       docker run -d --privileged --name buildkit moby/buildkit`,
+      channel,
+    );
   }
 
   async build(path: string, channel: string) {
-    runCmd(`railpack build ${path}`, channel, {
+    await this.install(channel);
+
+    return runCmd(`railpack build ${path}`, channel, {
       env: {
         BUILDKIT_HOST: "docker-container://buildkit",
       },
