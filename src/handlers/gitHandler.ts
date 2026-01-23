8bcf19ce529a0ad0c1fe519817319424aa161e74
aislxflames <proarghaop@gmail.com>
2026-01-23 17:39:51 +0530

    Add git handler for repository operations

---
+++ src/handlers/gitHandler.ts
@@ -0,0 +1,32 @@
+import { getGitApp } from "../lib/gitAppHandler.js";
+import {
+  createOctokitGit,
+  getInstallationAccessToken,
+  getInstallationId,
+} from "../lib/octokitGit.js";
+import { prisma } from "../lib/prisma.js";
+
+export const getCloneUrl = async (token: string, repo: string) => {
+  return `https://x-access-token:${token}@github.com/${repo}.git`;
+};
+
+export const generateGitToken = async (appId: number, email: string) => {
+  try {
+    const app = await getGitApp(email, appId);
+    const octokit = createOctokitGit(appId, app?.private_key!);
+    const installationId = await getInstallationId(octokit);
+    const token = await getInstallationAccessToken(octokit, installationId);
+
+    await prisma.githubApp.update({
+      where: {
+        app_id: appId,
+      },
+      data: {
+        cloneToken: token,
+      },
+    });
+    return token;
+  } catch (err) {
+    throw new Error("Failed to generate git token.");
+  }
+};
