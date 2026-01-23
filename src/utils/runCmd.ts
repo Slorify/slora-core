3f0a49a087b2ec0983c108d72516766393ef675f
aislxflames <proarghaop@gmail.com>
2026-01-23 17:41:18 +0530

    Update runCmd utility function

--- src/utils/runCmd.ts
+++ src/utils/runCmd.ts
@@ -6,7 +6,6 @@ interface CmdOptions {
   cwd?: string;
 }
 
-// GLOBAL LOG STORE (never reset unless server restarts)
 export const logStore: Record<string, string[]> = {};
 
 export async function runCmd(
@@ -14,22 +13,18 @@ export async function runCmd(
   channel: string,
   options: CmdOptions = {},
 ) {
-  // Initialize channel store
   if (!logStore[channel]) logStore[channel] = [];
 
   return run(
     cmd,
     (line: string) => {
-      // Initialize if not exists
       if (!logStore[channel]) logStore[channel] = [];
-      
-      // Save logs
+
       logStore[channel].push(line);
 
-      // Limit to last 500 logs (optional)
-      if (logStore[channel] && logStore[channel].length > 500) logStore[channel].shift();
+      if (logStore[channel] && logStore[channel].length > 500)
+        logStore[channel].shift();
 
-      // Emit to all connected clients
       io.emit(channel, line);
     },
     options,
